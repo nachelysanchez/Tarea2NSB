54ba13ab053035d7ce6d627909b2214982075b2b
nachelysanchez <nashely-5@hotmail.com>
2022-08-06 10:50:46 -0400

    Initial commit

---
+++ Clases/Operacion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2NSB
+{
+    class Operacion
+    {
+        protected int valor1;
+        public int Valor1
+        {
+            get
+            {
+                return valor1;
+            }
+            set
+            {
+                valor1 = value;
+            }
+        }
+
+        protected int valor2;
+        public int Valor2
+        {
+            get
+            {
+                return valor2;
+            }
+            set
+            {
+                valor2 = value;
+            }
+        }
+        protected int resultado;
+        public int Resultado
+        {
+            get
+            {
+                return resultado;
+            }
+            set
+            {
+                resultado = value;
+            }
+        }
+    }
+}
---
+++ Clases/Sumar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2NSB.Clases
+{
+    class Sumar : Operacion
+    {
+        public int operar(int v1, int v2)
+        {
+            valor1 = v1;
+            valor2 = v2;
+            return resultado = valor1 + valor2;
+        }
+    }
+}
